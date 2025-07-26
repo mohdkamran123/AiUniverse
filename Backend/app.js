394996739c58d267985dcf13b0bbfd1934eb6382
Mohd Kamran <128034463+mohdkamran123@users.noreply.github.com>
2025-07-26 11:52:07 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:3001', // Your React app
+  origin: 'https://aiuniverse-f.onrender.com', 
   credentials: true
 }));
 app.use(express.json());
